e72ce450e80d16dcc845a035dd025b80185f933f
Nicolas Brugneaux <nicolas@passbase.com>
2020-10-15 14:25:44 +0200

    release: v1.0.0

--- passbase/configuration.py
+++ passbase/configuration.py
@@ -254,5 +254,5 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.0.0\n"\
-               "SDK Package Version: 0.0.1".\
+               "SDK Package Version: 1.0.0".\
                format(env=sys.platform, pyversion=sys.version)
